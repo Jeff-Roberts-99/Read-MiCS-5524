c01426cc9dfd9738374efb28dcc049b1038e29fa
Jeff-Roberts-99 <96703711+Jeff-Roberts-99@users.noreply.github.com>
2024-09-13 21:26:54 -0500

    Added warmup sensor period

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Read-MiCS-5524
 
-Simple Arduino project to read from a MiCS-5524 air-quality (VOC) sensor. 
-Reads from the AO analog input pin once per second and outputs the current value to the serial output.
-Blinks a heartbeat LED twice per second.
+Simple read from the MiCS-5524 breakout board. Reads through A0 once per second.
+Reports elapsed time (in seconds) and sensor value from 0-100%.
+A0 is a 12-bit ADC (counts range from 0 to 4095).
+Also blinks a heartbeat LED twice per second.
+
+Sample output:
+Read-MiCS-5524 started up!
+timeNow = 4, sensorValue  = 19.88%
+timeNow = 5, sensorValue  = 3.52%
+timeNow = 6, sensorValue  = 3.64%
+timeNow = 7, sensorValue  = 3.25%
+timeNow = 8, sensorValue  = 3.81%
+timeNow = 9, sensorValue  = 3.88%
--- Read-MiCS-5524.ino
+++ Read-MiCS-5524.ino
@@ -1,3 +1,8 @@
+//Task intervals
+int TASK_HEARTBEAT_INTERVAL = 500;  //Blink on for 500 milliseconds
+int TASK_ADC_READ_INTERVAL = 250;  
+int SENSOR_WARMUP_INTERVAL = 90000; //90 seconds wait time for sensor to warm up
+
 struct Task {
     unsigned long lastRunTime;  // Last time the task was run
     unsigned long interval;     // Interval at which to run the task
@@ -6,8 +11,17 @@ struct Task {
 
 //global variables
 int ledHeartbeat = LED_BUILTIN;
-int adcInputPin = A0;
+int adcInputPin = A2;
 boolean BlinkOn = false;
+int sensorState;
+
+enum e_sensorState
+{
+    ssWarmingUp,
+    ssDetectingGas,
+    ssGasDetected
+};
+
 
 // Function prototypes
 void heartbeatTask();
@@ -16,8 +30,8 @@ void adcReadTask();
 // Task array
 Task tasks[] = 
 {
-    {0, 500, heartbeatTask},    // Heartbeat task with 500ms interval
-    {0, 1000, adcReadTask}      // ADC read task with 1000ms interval
+    {0, TASK_HEARTBEAT_INTERVAL, heartbeatTask},    // Heartbeat task with 500ms interval
+    {0, TASK_ADC_READ_INTERVAL, adcReadTask}      // ADC read task with 250ms interval
 };
 
 const int numberOfTasks = sizeof(tasks) / sizeof(tasks[0]);
@@ -27,9 +41,12 @@ void setup() {
   Serial.begin(115200);
   delay(3000);
   Serial.println("Read-MiCS-5524 started up!");
+  Serial.println("Waiting 90 seconds for sensor to warm up.");
+  Serial.println("Gas% delta% Spike");
 
   // initialize the LED pin as an output.
   pinMode(ledHeartbeat, OUTPUT);
+  digitalWrite (ledHeartbeat, false);
 
   // Set initial task times to current time
   unsigned long currentTime = millis();
@@ -37,6 +54,9 @@ void setup() {
   {
     tasks[i].lastRunTime = currentTime;
   }
+
+  //Set the state machine 
+  sensorState = ssWarmingUp;
 }
 
 void loop() {
@@ -46,6 +66,10 @@ void loop() {
   int sensorValue = 0;
   float percentValue;
 
+  if (timeNow > SENSOR_WARMUP_INTERVAL)
+  {
+      sensorState = ssDetectingGas;
+  }
 
   // Iterate through tasks
   for (int i = 0; i < numberOfTasks; i++) 
@@ -62,14 +86,53 @@ void loop() {
 
 void heartbeatTask() 
 {
-    digitalWrite (ledHeartbeat, BlinkOn);
-    BlinkOn = !BlinkOn;
+      if (toggleBlink>0)
+      {
+        digitalWrite (ledHeartbeat, BlinkOn);
+        BlinkOn = !BlinkOn;
+        toggleBlink--;  
+      }  
 }
 
 void adcReadTask() 
 {
-    int sensorValue = analogRead(adcInputPin);
-    float percentValue = (float)sensorValue / 4095.0 * 100.0;
-    Serial.println("Sensor Value = " + String(percentValue) + "%");
+      //Read from the sensor, calculate new value, calculate delta from last value
+      taskTime[tnAdcReadTask] += TASK_ADC_READ_INTERVAL;
+      sensorValue = analogRead(adcInputPin);
+      newValue = (float)sensorValue/4095.0*100.0;
+      if (lastValue > 0)
+      {
+        delta = (newValue - lastValue ) / lastValue;
+      }
+      else
+      {
+        delta = 0;
+      }
+      //Serial.println("timeNow = " + String(timeNow/1000) + ", newValue  = " + String(newValue) + "%" + ", delta = " + String(delta*100.0) +"%");
+
+      if (sensorState == ssDetectingGas)
+      {
+          //Check for high gas value, or high delta
+          Serial.print (newValue);
+          Serial.print (" ");
+          Serial.print(delta);
+          Serial.print (" ");
+  
+          //Alert if we drift over 8 or find a spike
+          SpikeFound = false;
+          if (newValue > 8) {SpikeFound = true;}
+          if ((newValue > 3) and (delta>0.3)) {SpikeFound = true;}
+          if (SpikeFound)
+          {
+            toggleBlink=9;
+            BlinkOn = false;
+            Serial.println(100);
+          }
+          else
+          {
+            Serial.println(0);
+          }
+      }
+      lastValue = newValue;
 }
 
